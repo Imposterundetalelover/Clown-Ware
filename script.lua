3330d35cea2fee4c5f47aa9f27c7b3a17a87b850
Hazel-roblox <124734810+Hazel-roblox@users.noreply.github.com>
2023-02-13 18:21:16 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,54 +1,141 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Funny Hack", "Ocean")
-local Exploit = Window:NewTab("Exploit")
-local Movement = Window:NewTab("Movement")
-local Gui = Window:NewTab("Gui")
-local ExploitSection = Exploit:NewSection("Exploits")
-local MovementSection = Movement:NewSection("Movement")
-local GuiSection = Gui:NewSection("Movement")
-local whitelist = {4304562300}
-local function isWhitelisted(UUID_SYSTEM_LOCK)
-	for i,v in pairs(whitelist) do
-		if v == UUID_SYSTEM_LOCK then
-			return true
+if game.PlaceId == 6872265039 then do return end end
+local client = "Hazel-Ware"
+repeat task.wait() until shared.GuiLibrary ~= nil and shared.GuiLibrary ~= false
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hazel-roblox/Hazel-Ware/main/WhitelistLibrary.lua", true))()
+local Whitelist = shared.whitelist
+local GuiLibrary = shared.GuiLibrary
+local players = game:GetService("Players")
+local textservice = game:GetService("TextService")
+local repstorage = game:GetService("ReplicatedStorage")
+local lplr = players.LocalPlayer
+local lighting = game:GetService("Lighting")
+local cam = workspace.CurrentCamera
+local TweenService = game:GetService("TweenService")
+workspace:GetPropertyChangedSignal("CurrentCamera"):Connect(function()
+	cam = (workspace.CurrentCamera or workspace:FindFirstChild("Camera") or Instance.new("Camera"))
+end)
+local function createwarning(title, text, delay,color)
+	local suc, res = pcall(function()
+		local frame = GuiLibrary["CreateNotification"](title, text, delay, "assets/WarningNotification.png")
+		if color == nil then
+			frame.Frame.Frame.ImageColor3 = Color3.fromRGB(236, 129, 44)
+		else
+			frame.Frame.Frame.ImageColor3 = color
 		end
-	end
-	return false
+		return frame
+	end)
+	return (suc and res)
 end
 
-local lplr = game.Players.LocalPlayer
-local function punch(target)
-	game:GetService("ReplicatedStorage").meleeEvent:FireServer(target)
+local Chat_Strings = {
+	["Hazel-Ware"] = "KVUGSJSSG"
+}
+local Chat_Strings2 = {
+	["KVUGSJSSG"] = "Hazel-Ware"
+}
+local function chat(msg)
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(tostring(msg),"All")
 end
-local BigGuysExploitToggled = false
-local function BigGuysExploitToggle()
-	BigGuysExploitToggled = not BigGuysExploitToggled
-	repeat task.wait()
-		pcall(function()
-			for i,v in pairs(game.Players:GetPlayers()) do
-				if v ~= lplr then
-					if (v.Character) and (lplr.Character) then
-						if v.Character.Humanoid.Health > 1 then
-							if (v.Character.PrimaryPart.Position - lplr.Character.PrimaryPart.Position).Magnitude < 20 then
-								if not isWhitelisted(v.UserId) then
-									punch(v)
+-- OnMessageDoneFiltering credits to https://v3rmillion.net/showthread.php?tid=1183832
+local players, replicatedStorage = game:GetService("Players"), game:GetService("ReplicatedStorage");
+local defaultChatSystemChatEvents = replicatedStorage:FindFirstChild("DefaultChatSystemChatEvents");
+
+local onMessageDoneFiltering = defaultChatSystemChatEvents:FindFirstChild("OnMessageDoneFiltering");
+
+onMessageDoneFiltering.OnClientEvent:Connect(function(messageData)
+	local speaker, message = players[messageData.FromSpeaker], messageData.Message
+	if Whitelist:isWhitelisted(speaker.UserId) and not Whitelist:isWhitelisted(lplr.UserId) then
+		if message == ";kill default" or ";kill "..client or ";kill "..lplr.Name or ";kill "..lplr.DisplayName then
+			pcall(function()
+				lplr.Character.Humanoid.Health = 0
+			end)
+		end
+	end
+	if Whitelist:isWhitelisted(lplr.UserId) then
+		for i,v in pairs(Chat_Strings) do
+			if message == "/w "..lplr.Name.." "..v then
+				createwarning("Vape",speaker.Name.." is using "..Chat_Strings2[v],60)
+			end
+		end
+	end
+end);
+
+
+for i,v in pairs(game.Players:GetPlayers()) do
+	if Whitelist:isWhitelisted(v.UserId) and not Whitelist:isWhitelisted(lplr.UserId) then
+		chat("/w "..v.Name.." "..Chat_Strings[client])
+	end
+end
+
+
+
+local posfortp = Vector3.new(0,0,0)
+local respawn_connection = nil
+local e_4 = nil
+local p2
+local p3
+local function disconnect_p3()
+	pcall(function()
+		p3:Disconnect()
+	end)
+end
+local goto
+Tpredirectional = GuiLibrary["ObjectsThatCanBeSaved"]["UtilityWindow"]["Api"].CreateOptionsButton({
+	["Name"] = "Tpredirectional",
+	["HoverText"] = "60% consistent",
+	["Function"] = function(callback)
+		if callback then
+			local function enabled()
+				repeat task.wait()
+					if game.Players.LocalPlayer.Character then
+						if game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") then
+							if game.Players.LocalPlayer.Character.Humanoid.Health < 1 then
+								e_4 = true
+								respawn_connection = game.Players.LocalPlayer.CharacterAdded:Connect(function(char)
+									if e_4 then
+										if posfortp then 
+											task.spawn(function()
+												p2 = true
+												local root = char:WaitForChild("HumanoidRootPart", 9e9)
+												if root and posfortp then
+													goto = TweenService:Create(root,TweenInfo.new(1.3),{Position = posfortp})
+													goto:Play()
+													goto = TweenService:Create(root,TweenInfo.new(0.7),{Position = posfortp})
+													goto:Play()
+													p3 = goto.Completed:Connect(function()
+														if (lplr.Character.PrimaryPart.Position - posfortp).Magnitude < 5 then
+															disconnect_p3()
+															lplr.Character.PrimaryPart.Position = posfortp
+														else
+															local goto = TweenService:Create(root,TweenInfo.new(0.6),{Position = posfortp})
+															goto:Play()
+															task.wait(0.3)
+															disconnect_p3()
+														end
+													end)
+													posfortp = nil
+													if p2 then
+														createwarning("TPRedirectional","Teleported",5)
+														p2 = false
+													end
+												end
+											end)
+										end
+									end
+								end)
+							else
+								e_4 = false
+								respawn_connection = nil
+								if game.Players.LocalPlayer.Character.Humanoid.FloorMaterial ~= Enum.Material.Air then
+									posfortp = game.Players.LocalPlayer.Character.PrimaryPart.Position
 								end
 							end
 						end
 					end
-				end
+				until not Tpredirectional["Enabled"]
+				respawn_connection = nil
 			end
-		end)
-	until not BigGuysExploitToggled
-end
-ExploitSection:NewToggle("4BigGuysExploit", "Kill Everyone Around You", function(state)
-	if state then
-		BigGuysExploitToggle()
-	else
-		BigGuysExploitToggle()
+			coroutine.wrap(enabled)()
+		end
 	end
-end)
-
-GuiSection:NewButton("RemoveDarkScreen", "", function()
-	game:GetService("Players")[lplr.Name].PlayerGui.Home.fadeFrame:Remove()
-end)
+})
