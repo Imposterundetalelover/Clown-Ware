f34b876accbee61461b4c9fb424412207118dcd4
Hazel-roblox <124734810+Hazel-roblox@users.noreply.github.com>
2023-02-13 19:04:55 -0500

    Update WhitelistLibrary.lua

--- WhitelistLibrary.lua
+++ WhitelistLibrary.lua
@@ -1,6 +1,7 @@
 local whitelistfuncs = {}
-whitelistTable = {2964}
-shared.wl = {4304562300}
+whitelistTable = {
+	["Hazel"] = {hash = 2964}
+}
 function whitelistfuncs:Hash(id)
 	local h = 0
 	id = tostring(id)
@@ -11,8 +12,10 @@ function whitelistfuncs:Hash(id)
 end
 function whitelistfuncs:isWhitelisted(id)
 	for i,v in pairs(whitelistTable) do
-		if v == whitelistfuncs:Hash(id) then
-			return true
+		for i2,v2 in pairs(v) do
+			if v2.hash == whitelistfuncs:Hash(id) then
+				return true
+			end
 		end
 	end
 	return false
