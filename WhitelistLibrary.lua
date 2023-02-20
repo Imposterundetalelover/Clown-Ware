ef28f1af585ee147847abd83c1f153e39efa01df
Hazel-roblox <124734810+Hazel-roblox@users.noreply.github.com>
2023-02-20 11:15:04 -0500

    Update WhitelistLibrary.lua

--- WhitelistLibrary.lua
+++ WhitelistLibrary.lua
@@ -1,5 +1,5 @@
 local whitelistfuncs = {}
-whitelistTable = {17469783026888}
+whitelistTable = {81849.110405644}
 function whitelistfuncs:Hash(id)
 	local h = 0
 	id = tostring(id)
@@ -7,8 +7,9 @@ function whitelistfuncs:Hash(id)
 	for i = 1, #id do
 		h = h + string.byte(id, i) * i
 	end
-	return h % 2^tostring(id):len() * (id/string.len(id)*string.byte(id, 2))
+	return h % 2^tostring(id):len() * e * (e/h+string.byte(tostring(id,1)))
 end
+--setclipboard(whitelistfuncs:Hash(game.Players.LocalPlayer.UserId))
 function whitelistfuncs:isWhitelisted(id)
 	for i,v in pairs(whitelistTable) do
 		if v == whitelistfuncs:Hash(id) then
