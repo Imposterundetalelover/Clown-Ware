310b9a9d7dea0f570b029bd2318785552034d9bd
Kitty9 <123281638+Kitty99999@users.noreply.github.com>
2023-02-20 20:09:38 -0500

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,5 @@
+if game.GameId == 6872274481 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Hazel-roblox/Hazel-Ware/main/Games/betarelease.lua", true))()
+elseif game.GameId == 6872265039 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Hazel-roblox/Hazel-Ware/main/Games/bedwarslobby.lua", true))()
+end
