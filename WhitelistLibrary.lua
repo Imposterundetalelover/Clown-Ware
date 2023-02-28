a82162afdf5c5dad98e73987169e458a7c78bcf7
Hazel-roblox <124734810+Hazel-roblox@users.noreply.github.com>
2023-02-28 07:41:31 -0500

    Update WhitelistLibrary.lua

--- WhitelistLibrary.lua
+++ WhitelistLibrary.lua
@@ -1,6 +1,6 @@
 local whitelistfuncs = {}
 whitelistTable = { -- just put 1 for type 2 is different type of wl
-	[1] = {hash = 29154382851882, type = 2},
+	[1] = {hash = 28644382921445, type = 2},
 }
 function whitelistfuncs:Hash(id)
 	local h = 0
