a47fac72fedd8e14e91c4366e0f9dd670b1b8f07
Imposterundetalelover <112672496+Imposterundetalelover@users.noreply.github.com>
2023-03-28 12:09:14 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 if game.GameId == 6872274481 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Hazel-roblox/Hazel-Ware/main/Games/betarelease.lua", true))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Imposterundetalelover/Clown-Ware/main/Games/betarelease.lua", true))()
 elseif game.GameId == 6872265039 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Hazel-roblox/Hazel-Ware/main/Games/bedwarslobby.lua", true))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Imposterundetalelover/Clown-Ware/main/Games/bedwarslobby.lua", true))()
 end
